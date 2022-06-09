040a8a98a7719fc2009a0ecc7f640f6e445424dc
Vladislav Todorov <85580570+Vladddi@users.noreply.github.com>
2022-06-09 22:27:11 +0300

    Demo_1
    upload attempt "Hello_SoftUni"

---
+++ p01_Hello_SoftUni.java
@@ -0,0 +1,10 @@
+package First_Steps_In_Coding;
+
+public class p01_Hello_SoftUni {
+    public static void main(String[] args) {
+        // code psvm
+        System.out.println("Hello SoftUni");
+        //sout
+    }
+
+}
