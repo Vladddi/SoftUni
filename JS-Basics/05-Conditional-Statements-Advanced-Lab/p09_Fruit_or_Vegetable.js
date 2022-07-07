8c94eecc27afd742297d1cfaad75ce3268b6eb13
Vladislav Todorov <85580570+Vladddi@users.noreply.github.com>
2022-07-08 00:15:19 +0300

    Add p09_Fruit_or_Vegetable
    Solution

---
+++ JS-Basics/05-Conditional-Statements-Advanced-Lab/p09_Fruit_or_Vegetable.js
@@ -0,0 +1,30 @@
+function fruirOrVegetable(input) {
+    let something = input[0];
+
+    switch (something) {
+        case 'banana':
+        case 'apple':
+        case 'kiwi':
+        case 'cherry':
+        case 'lemon':
+        case 'grapes':
+            console.log('fruit');
+            break;
+
+        case 'tomato':
+        case 'cucumber':
+        case 'pepper':
+        case 'carrot':
+            console.log('vegetable');
+            break;
+
+        default:
+            console.log('unknown');
+            break;
+    }
+}
+
+fruirOrVegetable(["banana"]);
+fruirOrVegetable(["apple"]);
+fruirOrVegetable(["tomato"]);
+fruirOrVegetable(["water"]);
