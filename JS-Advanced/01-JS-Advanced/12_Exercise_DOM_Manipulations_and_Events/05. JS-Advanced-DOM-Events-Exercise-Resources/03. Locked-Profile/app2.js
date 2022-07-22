b4a02bab5d12197d00701dae92e2d1801b68d41c
Vladislav Todorov <85580570+Vladddi@users.noreply.github.com>
2022-07-22 17:13:03 +0300

    Create app2.js
    Solution 2 with locked-gray-button

---
+++ JS-Advanced/01-JS-Advanced/12_Exercise_DOM_Manipulations_and_Events/05. JS-Advanced-DOM-Events-Exercise-Resources/03. Locked-Profile/app2.js
@@ -0,0 +1,48 @@
+function lockedProfile() {
+    // add toggle event listener to all profile buttons
+
+    // find associated profile details
+    // if it's visivle -> hide it and display label 'Show more'
+    // otherwise -> show it and display label 'Hide it'
+
+
+    Array.from(document.querySelectorAll('.profile button')).forEach(b => b.addEventListener('click', onToggle));
+
+    function onToggle(e) {
+        const profile = e.target.parentElement;
+        const isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked;
+
+
+        if (isActive) {
+            const infoDiv = profile.querySelector('div');
+            //     const infoDiv = Array
+            //         .from(e.target.parentElement.querySelectorAll('div'))
+            //         .find(d => d.id.includes('HiddenFields'));
+
+            if (e.target.textContent == 'Show more') {
+                infoDiv.style.display = 'block';
+                e.target.textContent = 'Hide it';
+            } else {
+                infoDiv.style.display = '';
+                e.target.textContent = 'Show more';
+            }
+        }
+    }
+
+    //////////////////////////////////////
+
+    const radio = Array.from(document.querySelectorAll('input[type="radio"]'));
+        for (let input of radio) {
+            input.addEventListener('click', onLockToggle);
+        }
+
+        function onLockToggle(e) {
+            const button = e.target.parentElement.querySelector('button');
+
+            if (e.target.value == 'lock') {
+                button.disabled = true;
+            } else {
+                button.disabled = false;
+            }
+        }
+    }
