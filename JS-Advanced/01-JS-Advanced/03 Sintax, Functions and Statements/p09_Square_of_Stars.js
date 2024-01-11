2c1fc879b8421fb6f3cfe2c5d54fc47786333122
Vladislav Todorov <85580570+Vladddi@users.noreply.github.com>
2024-01-11 10:38:11 +0200

    Update p09_Square_of_Stars.js

--- JS-Advanced/01-JS-Advanced/03 Sintax, Functions and Statements/p09_Square_of_Stars.js
+++ JS-Advanced/01-JS-Advanced/03 Sintax, Functions and Statements/p09_Square_of_Stars.js
@@ -2,18 +2,26 @@ function rectangle(input) {
     let typeInput = typeof (input);
 
     if (typeInput === 'number') {
-        for (let i = 0; i <= input; i++) {
-            console.log('* ');
+        for (let i = 0; i < input; i++) {
+            let row = '';
+            for (let j = 0; j < input; j++) {
+                row += '* ';
+            }
+            console.log(row);
         }
-        // if (input == '') {
-        //     for(let i = 0; i <= 5; i++) {
-        //         for(let j = 0; j <= 5; i++) {
-        //             console.log('* ');
-        //         }
-        //     }
-        // }
+    } else {
+        for (let i = 0; i < 5; i++) {
+            let row = '';
+            for (let j = 0; j < 5; j++) {
+                row += '* ';
+            }
+            console.log(row);
+        }
     }
-
-
 }
-rectangle('2');
+
+ rectangle('5');
+ rectangle(1);
+ rectangle(2);
+ rectangle(5);
+ rectangle(7);
