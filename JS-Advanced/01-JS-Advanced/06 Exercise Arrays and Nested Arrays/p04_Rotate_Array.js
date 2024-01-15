592003d76ab01bc509bacdaf6b0e51452521234b
Vladislav Todorov <85580570+Vladddi@users.noreply.github.com>
2024-01-15 21:07:57 +0200

    Update p04_Rotate_Array.js

--- JS-Advanced/01-JS-Advanced/06 Exercise Arrays and Nested Arrays/p04_Rotate_Array.js
+++ JS-Advanced/01-JS-Advanced/06 Exercise Arrays and Nested Arrays/p04_Rotate_Array.js
@@ -1,11 +1,20 @@
 function solve(arr, rotations) {
-    for (let i = 0; i < rotations; i++) {
-        const element = arr.pop();
-        arr.unshift(element);
-    }
+    
+let circle = rotations % arr.length;
 
-    return arr.join(' ');
+    for (let i = 0; i < circle; i++) {
+        arr.unshift(arr.pop());
+    }
+    console.log(arr.join(' '));
 }
+// function solve(arr, rotations) {
+//     for (let i = 0; i < rotations; i++) {
+//         const element = arr.pop();
+//         arr.unshift(element);
+//     }
+
+//     return arr.join(' ');
+// }
 
 console.log(solve(['Banana',
     'Orange',
